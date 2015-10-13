b4736394a7b9353db8e2e1918aaf1eb435d0134c
ryan quinn <ryanwilliamquinn@gmail.com>
2015-10-12 21:08:05 -0400

    add .equal for better error message

--- test/unit/StringReverseService.test.js
+++ test/unit/StringReverseService.test.js
@@ -3,13 +3,12 @@ var StringReverseService = require('../../api/services/StringReverseService');
 
 describe('string reverse service', function() {
 
-  it('should reverse an input string', function(done) {
-    assert(StringReverseService.reverse('winnie') == 'einniw');
-    done();
+  it('should reverse an input string', function() {
+    assert.equal(StringReverseService.reverse('winnie'), 'einniw');
   });
 
   it('should return an empty string for invalid input', function () {
-    assert(StringReverseService.reverse() === '');
+    assert.equal(StringReverseService.reverse(), '');
   });
 
 });
